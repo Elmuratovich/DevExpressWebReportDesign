3919e167fafc3cc6c258e83690435ec14b387ebe
Makhmud <makhmudkuzikaev@gmail.com>
2024-04-30 16:22:06 +0600

    start

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DevExpressReport.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+using DevExpress.AspNetCore;
+using DevExpress.AspNetCore.Reporting;
+using Microsoft.Extensions.FileProviders;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+// Config of DevExpress
+builder.Services.AddControllersWithViews().AddJsonOptions(options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
+builder.Services.AddDevExpressControls();
+
+builder.Services.ConfigureReportingServices(configurator =>
+{
+    configurator.DisableCheckForCustomControllers();
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(Path.Combine(app.Environment.ContentRootPath, "node_modules")),
+    RequestPath = "/node_modules"
+});
+
+app.UseRouting();
+
+app.UseAuthorization();
+app.UseDevExpressControls();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
